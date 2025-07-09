07603ff7b588ab1657bc291d5bbb79ef611a359e
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-07-09 11:14:42 +0300

    create grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 function init(){
+    // All Code goes here
+    const gridElem = document.querySelector('.grid')
 
+    function createGrid(){
+        //for every cell we need create a div
+        //append this cell to our grid
+        for (let i = 0; i < 100; i++){
+            const cell = document.createElement('div')
+            cell.textContent = i 
+            gridElem.appendChild(cell)
+        }
+    }
+    createGrid()
 }
 
 document.addEventListener('DOMContentLoaded',init)
