b68d3f3c5fbfb29d95a8dc8e351536a084bd0841
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-07-09 13:32:24 +0300

    added random duck position and display

--- scripts/app.js
+++ scripts/app.js
@@ -6,12 +6,30 @@ function init(){
     const gridWidth = 10
     const numberOfCells = gridWidth * gridWidth
 
+    let duckPosition = 87
+
+    function addDuck(){
+        cells[duckPosition].classList.add('duck')
+    }
+    
+    function removeDuck(){
+        cells[duckPosition].classList.remove('duck')
+    }
+
+    function play(){
+        setInterval(() => {
+            removeDuck()
+            duckPosition = Math.floor(Math.random() * numberOfCells)
+            addDuck()
+        },1000)
+    }
+
     function createGrid(){
         //for every cell we need create a div
         //append this cell to our grid
         for (let i = 0; i < numberOfCells; i++){
             const cell = document.createElement('div')
-            cell.classList.add('duck')
+            // cell.classList.add('duck')
             cell.textContent = i 
             cells.push(cell)
             gridElem.appendChild(cell)
@@ -19,6 +37,7 @@ function init(){
         console.log(cells)
     }
     createGrid()
+    play()
 }
 
 document.addEventListener('DOMContentLoaded',init)
